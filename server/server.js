af6aad8d4eee603fb8335495f401573d3ba43a0b
Brayan Huaman Pereda <brayan92xd@gmail.com>
2018-10-17 12:47:35 -0500

    edit readme

---
+++ server/server.js
@@ -0,0 +1,138 @@
+ // server/server.js
+
+  let express = require("express");
+  let graphqlHTTP = require("express-graphql");
+  let { buildSchema } = require("graphql");
+  let cors = require("cors");
+  let Pusher = require("pusher");
+  let bodyParser = require("body-parser");
+  let Multipart = require("connect-multiparty");
+
+  // Construct a schema, using GraphQL schema language
+  let schema = buildSchema(`
+      type User {
+        id : String!
+        nickname : String!
+        avatar : String!
+      }
+      type Post {
+          id: String!
+          user: User!
+          caption : String!
+          image : String!
+      }
+      type Query{
+        user(id: String) : User!
+        post(user_id: String, post_id: String) : Post!
+        posts(user_id: String) : [Post]
+      }
+  `);
+
+
+  // Maps id to User object
+  let userslist = {
+    a: {
+      id: "a",
+      nickname: "Brayan Huaman Pereda",
+      avatar: "https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/28337276_10155394741062333_7368194779110885507_o.jpg?_nc_cat=101&_nc_eui2=AeEAdJHKzqUQKtBhfEI6378kBBiBa-eDMvCXtmD-pJv_PJcVqkR9CjRRIdD27rIzy_OcYFtfPKQ6o-IsxCWKv9Rew7C3gDSf0H99afYDGQPvHQ&_nc_ht=scontent.flim5-4.fna&oh=4cddfc02f187bad63d3937fa4e1ea0db&oe=5C46301B"
+    },
+    b: {
+      id: "b",
+      nickname: "BHP",
+      avatar:
+        "https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/28337276_10155394741062333_7368194779110885507_o.jpg?_nc_cat=101&_nc_eui2=AeEAdJHKzqUQKtBhfEI6378kBBiBa-eDMvCXtmD-pJv_PJcVqkR9CjRRIdD27rIzy_OcYFtfPKQ6o-IsxCWKv9Rew7C3gDSf0H99afYDGQPvHQ&_nc_ht=scontent.flim5-4.fna&oh=4cddfc02f187bad63d3937fa4e1ea0db&oe=5C46301B"
+    }
+  };
+
+  let postslist = {
+    a: {
+      a: {
+        id: "a",
+        user: userslist["a"],
+        caption: "Paris",
+        image: "https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/28337276_10155394741062333_7368194779110885507_o.jpg?_nc_cat=101&_nc_eui2=AeEAdJHKzqUQKtBhfEI6378kBBiBa-eDMvCXtmD-pJv_PJcVqkR9CjRRIdD27rIzy_OcYFtfPKQ6o-IsxCWKv9Rew7C3gDSf0H99afYDGQPvHQ&_nc_ht=scontent.flim5-4.fna&oh=4cddfc02f187bad63d3937fa4e1ea0db&oe=5C46301B"
+      },
+      b: {
+        id: "b",
+        user: userslist["a"],
+        caption: "Paris",
+        image:
+          "https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/28337276_10155394741062333_7368194779110885507_o.jpg?_nc_cat=101&_nc_eui2=AeEAdJHKzqUQKtBhfEI6378kBBiBa-eDMvCXtmD-pJv_PJcVqkR9CjRRIdD27rIzy_OcYFtfPKQ6o-IsxCWKv9Rew7C3gDSf0H99afYDGQPvHQ&_nc_ht=scontent.flim5-4.fna&oh=4cddfc02f187bad63d3937fa4e1ea0db&oe=5C46301B"
+      },
+      c: {
+        id: "c",
+        user: userslist["a"],
+        caption: "Paris",
+        image: "https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/28337276_10155394741062333_7368194779110885507_o.jpg?_nc_cat=101&_nc_eui2=AeEAdJHKzqUQKtBhfEI6378kBBiBa-eDMvCXtmD-pJv_PJcVqkR9CjRRIdD27rIzy_OcYFtfPKQ6o-IsxCWKv9Rew7C3gDSf0H99afYDGQPvHQ&_nc_ht=scontent.flim5-4.fna&oh=4cddfc02f187bad63d3937fa4e1ea0db&oe=5C46301B"
+      },
+      d: {
+        id: "d",
+        user: userslist["a"],
+        caption: "Paris",
+        image: "https://scontent.flim5-4.fna.fbcdn.net/v/t31.0-8/28337276_10155394741062333_7368194779110885507_o.jpg?_nc_cat=101&_nc_eui2=AeEAdJHKzqUQKtBhfEI6378kBBiBa-eDMvCXtmD-pJv_PJcVqkR9CjRRIdD27rIzy_OcYFtfPKQ6o-IsxCWKv9Rew7C3gDSf0H99afYDGQPvHQ&_nc_ht=scontent.flim5-4.fna&oh=4cddfc02f187bad63d3937fa4e1ea0db&oe=5C46301B"
+      }
+    }
+  };
+
+   // The root provides a resolver function for each API endpoint
+   let root = {
+    user: function({ id }) {
+      return userslist[id];
+    },
+    post: function({ user_id , post_id }) {
+      return postslist[user_id][post_id];
+    },
+    posts: function({ user_id }){
+      return Object.values(postslist[user_id]);
+    }
+  };
+
+  // Configure Pusher client
+  let pusher = new Pusher({
+    appId: '624652',
+    key: '143bcfe9f03278041140',
+    secret: '3b09dfe32946367ea20f',
+    cluster: 'us2',
+    encrypted: true
+  });
+
+  let app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
+
+   //add Middleware
+   let multipartMiddleware = new Multipart();
+
+   app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true
+    })
+  );
+
+   //trigger add a new post
+   app.post('/newpost', multipartMiddleware, (req,res) => {
+     // create a sample post
+     let post = {
+       user : {
+         nickname : req.body.name,
+         avatar : req.body.avatar
+       },
+       image : req.body.image,
+       caption : req.body.caption
+     };
+
+     // trigger pusher event
+     pusher.trigger("posts-channel", "new-post", {
+       post
+     });
+
+     return res.json({status : "Post created"});
+   });
+
+
+  // set application port
+  app.listen(4000);
+  console.log("Running a GraphQL API server at localhost:4000/graphql");
--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "react-apollo";
 import './App.css';
 
+import Pusher from 'pusher-js';
+
+import Header from './components/Header';
+import Posts from './components/Posts';
+
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.pusher = new Pusher("143bcfe9f03278041140", {
+      cluster: 'eu',
+      encrypted: true
+    });
+  }
+
+  componentDidMount(){
+    if ('actions' in Notification.prototype) {
+      console.log('You can enjoy notification feature');
+    } else {
+      console.log('Sorry notifications are NOT supported on your browser');
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+          <section className="App-main">
+            {/* pass the pusher object and apollo to the posts component */}
+            <Posts pusher={this.pusher} apollo_client={client} />
+          </section>
+        </div>
+      </ApolloProvider>
     );
   }
-}
 
+}
 export default App;
